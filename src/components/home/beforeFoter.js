4fa0c0d12f329e80a98a26aecfd574ae7fed70a8
ansab210 <rajpootansab2@gmail.com>
2025-02-11 11:32:12 -0800

    Full home

--- src/components/home/beforeFoter.js
+++ src/components/home/beforeFoter.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import styles from "@/components/home/styles/beforeFoter.module.css";
 import { Input, Button } from "antd";
-import { Row, Col } from "antd";
 
 function beforeFoter() {
   const [isClicked, setIsClicked] = useState(false);
@@ -21,33 +20,48 @@ function beforeFoter() {
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry.
           </p>
-          <Row justify="center">
-            <Col xs={24} sm={18} md={16} lg={12}>
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <Input
-                  placeholder="Enter your email id"
-                  style={{
-                    flex: 1,
-                    height: "50px",
-                    borderRadius: "5px 0 0 5px",
-                    fontSize: "16px",
-                  }}
-                />
-                <Button
-                  type="primary"
-                  style={{
-                    height: "50px",
-                    background: "black",
-                    color: "white",
-                    borderRadius: "0 5px 5px 0",
-                    fontSize: "16px",
-                  }}
-                >
-                  SUBSCRIBE
-                </Button>
-              </div>
-            </Col>
-          </Row>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "20px",
+            }}
+          >
+            <Input
+              placeholder="Enter email id"
+              type="email"
+              style={{
+                width: "50%",
+                padding: "10px 20px",
+                marginRight: "0px",
+                borderRadius: "0px",
+                outline: "none", // Remove the default outline
+                border: "1px solid #000000", // Add a black border
+              }}
+              onFocus={(e) => {
+                e.target.style.border = "1px solid #000000"; // Change border on focus
+                e.target.style.bordershadow = " #000000";
+              }}
+              onBlur={(e) => {
+                e.target.style.border = "1px solid #000000"; // Revert border on blur
+              }}
+            />
+            <Button
+              type="primary"
+              style={{
+                backgroundColor: isClicked ? "transparent" : "#000000", // Change background on click
+                padding: "25px 25px",
+                borderRadius: "0px",
+                border: isClicked ? "1px solid black" : "1px solid #000000", // Add border on click
+                color: isClicked ? "black" : "white", // Change text color on click
+                transition:
+                  "background-color 0.2s ease, color 0.2s ease, border 0.2s ease", // Smooth transition
+              }}
+              onClick={handleClick}
+            >
+              SUBSCRIBE
+            </Button>
+          </div>
         </div>
       </div>
     </>
